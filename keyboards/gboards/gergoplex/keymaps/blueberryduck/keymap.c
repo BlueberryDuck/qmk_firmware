99507905e6152f08eae8e89c52190236e702564b
BuildTools <unconfigured@null.spigotmc.org>
2021-09-25 16:56:03 +0200

    removed reset

--- keyboards/gboards/gergoplex/keymaps/blueberryduck/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/blueberryduck/keymap.c
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .------------------------------------.	.-----------------------------.
  * |  F1   |  F2  |  F3   |  F4   | F5  |	|NLCK |  7  |  8  |  9  |GAME |
  * |-------+------+-------+-------+-----|	|-----+-----+-----+-----------|
- * |META/F6|ALT/F7|SHFT/F8|CTRL/F9| F10 |	|     |  4  |  5  |  6  |  0  | 
+ * |META/F6|ALT/F7|SHFT/F8|CTRL/F9| F10 |	| ESC |  4  |  5  |  6  |  0  | 
  * |-------+------+-------+-------+-----|	|-----+-----+-----+-----------|
- * |  F11  | F12  |       |       |RESET|	|     |  1  |  2  |  3  |  ,  |
+ * |  F11  | F12  |       |       |     |	|     |  1  |  2  |  3  |  ,  |
  * '------------------------------------'	'-----------------------------'
  * 				      .-----------------.	.-----------------.   
  *  			      |     |     |     |	|     |     |     |
@@ -90,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [NUMB] = LAYOUT_split_3x5_3(
     KC_F1,				KC_F2,				KC_F3,				KC_F4,				KC_F5,		KC_NLCK,	KC_P7,	KC_P8,	KC_P9,	TG(GAME),
-    MT(MOD_LGUI,KC_F6),	MT(MOD_LALT,KC_F7),	MT(MOD_LSFT,KC_F8),	MT(MOD_LCTL,KC_F9),	KC_F10,		KC_TRNS,	KC_P4,	KC_P5,	KC_P6,	KC_P0,
-    KC_F11,				KC_F12,				KC_TRNS,			KC_TRNS,			RESET,		KC_TRNS,	KC_P1,	KC_P2,	KC_P3,	KC_PDOT,
+    MT(MOD_LGUI,KC_F6),	MT(MOD_LALT,KC_F7),	MT(MOD_LSFT,KC_F8),	MT(MOD_LCTL,KC_F9),	KC_F10,		KC_ESC,		KC_P4,	KC_P5,	KC_P6,	KC_P0,
+    KC_F11,				KC_F12,				KC_TRNS,			KC_TRNS,			KC_TRNS,	KC_TRNS,	KC_P1,	KC_P2,	KC_P3,	KC_PDOT,
 
 	KC_TRNS,	KC_TRNS,	KC_TRNS,  															KC_TRNS,	KC_TRNS,	KC_TRNS
     ),
